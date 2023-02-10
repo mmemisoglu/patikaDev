0e01fd68617f4bfc1d01512404a526d2b1d0060a
Mustafa Memişoğlu <m.memisoglu04@gmail.com>
2023-02-11 00:51:43 +0300

    Ödev-2 klasörlendi

---
+++ odev_2/odev_2.js
@@ -0,0 +1,31 @@
+//Ödev-2
+
+const post = ["Post1", "Post2", "Post3"]
+
+var postSort = () => {
+    for (var i = 0; i < post.length; i++) {
+        console.log(post[i]);
+    }
+}
+
+var addPost = (addPost) => {
+    const promise = new Promise((resolve, reject) => {
+        if (addPost == " ") reject("Boş post atılamaz")
+        else {
+            post[post.length] = addPost;
+            resolve("Yeni post eklendi.");
+        }
+    })
+    return promise;
+}
+
+async function asyncPost(){
+    try{
+        await addPost("New Post");
+        postSort();
+    }catch(err){
+        console.log(err);
+    }
+}
+asyncPost();
+
