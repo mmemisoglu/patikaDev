b8bb6c7a185c6a6c1206f89c1e2d03b0c96fb4db
Mustafa Memişoğlu <m.memisoglu04@gmail.com>
2023-02-11 05:29:30 +0300

    Ödev-5 yapıldı

--- index.mjs
+++ index.mjs
@@ -1,24 +1,27 @@
-import http from "http"
+import http from "http";
 
 const server = http.createServer((req,res) => {
     const url = req.url;
     if(url === '/'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write("Index Sayfasi");
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write("<h2>Anasayfa sayfasına hoş geldiniz.</h2>")
+    }else if(url === '/index'){
+        res.writeHead(200,{'Content-Type': 'text/html'})
+        res.write("<h2>Index sayfasına hoş geldiniz.</h2>")
     }else if(url === '/about'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write("About sayfası")
+        res.writeHead(200,{'Content-Type': 'text/html'})
+        res.write("<h2>Hakkimizda sayfasına hoş geldiniz.</h2>")
     }else if(url === '/contact'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write("Contact sayfası")
+        res.writeHead(200,{'Content-Type': 'text/html'})
+        res.write("<h2>Iletisim sayfasına hoş geldiniz.</h2>")
     }else{
-        res.writeHead(404, {'Content-Type': 'text/html'})
-        res.write("<h1>Sayfa bulunamadı</h1>")
+        res.writeHead(404,{'Content-Type': 'text/html'})
+        res.write("<h2>Sayfa bulunamadı</h2>")
     }
-    
-    res.end();
-});
-const port = 3000
+    res.end()
+})
+
+const port = 5001
 server.listen(port, () => {
     console.log(`Sunucu port ${port} de başlatıldı.`)
 })
